188c8fd45ded66b98e759f3ce99836de06f7129d
Annbabaika <annbabaika@mail.ru>
2022-06-24 16:03:35 +0300

    Initial Commit

---
+++ UiSliderColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiSliderColor
+//
+//  Created by Anna Prilipkina on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
